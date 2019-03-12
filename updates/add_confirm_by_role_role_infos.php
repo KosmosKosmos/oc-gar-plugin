9a92a1dad7c692d1df7244aad355c626714d7d9a
Vladimir Bubar <vladimir.bubar@kosmoskosmos.de>
2019-03-12 15:57:14 +0100

    Confirm by role flag.

---
+++ updates/add_confirm_by_role_role_infos.php
@@ -0,0 +1,24 @@
+<?php namespace Andosto\EventManager\Updates;
+
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+class AddConfirmByRoleRoleInfos extends Migration
+{
+    public function up()
+    {
+        Schema::table('kosmoskosmos_gar_role_infos', function(Blueprint $table) {
+            $table->boolean('confirm_by_role')->default(true);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('kosmoskosmos_gar_role_infos', function(Blueprint $table) {
+			if (Schema::hasColumn('kosmoskosmos_gar_role_infos', 'confirm_by_role')) {
+				$table->dropColumn('confirm_by_role');
+			}
+        });
+    }
+}
