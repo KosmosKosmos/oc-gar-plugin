3383fdc2e6f034c1b2990bbfa0a17b21edb8753a
Vladimir Bubar <vladimir.bubar@kosmoskosmos.de>
2019-03-14 11:42:52 +0100

    Generate and store PDF on confirm.

--- controllers/Confirms.php
+++ controllers/Confirms.php
@@ -4,9 +4,15 @@ use Backend\Facades\Backend;
 use Backend\Facades\BackendAuth;
 use BackendMenu;
 use Backend\Classes\Controller;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\URL;
 use KosmosKosmos\GAR\Models\Confirm;
+use KosmosKosmos\GAR\Models\GARSettings;
 use KosmosKosmos\GAR\Models\RoleInfo;
+use Renatio\DynamicPDF\Classes\PDF;
 
 /**
  * Confirms Back-end Controller
@@ -42,6 +48,26 @@ class Confirms extends Controller
                     'confirmable_id' => $roleInfo->confirm_by_role ? $role->id : $user->id,
                     'confirmable_type' => $roleInfo->confirm_by_role ? get_class($role) : get_class($user),
                 ]);
+
+                if (!File::exists(storage_path('kosmoskosmos'))) {
+                    File::makeDirectory(storage_path('kosmoskosmos'));
+                }
+
+                if (!File::exists(storage_path('kosmoskosmos/signed'))) {
+                    File::makeDirectory(storage_path('kosmoskosmos/signed'));
+                }
+
+                $filename = 'signed_'.($roleInfo->confirm_by_role ? 'role_'.$role->id : 'user_'.$user->id).'.pdf';
+                $gar = GARSettings::get('gar_text');
+
+                PDF::loadTemplate('gar-confirm',
+                        ['gar' => $gar,
+                         'ip' => Request::ip(),
+                         'date' => Carbon::now()->format('d.m.Y H:i:s'),
+                         'roleInfo' => $roleInfo,
+                         'url' => URL::to('/')
+                        ]
+                )->save(storage_path('kosmoskosmos/signed/'.$filename));
 
                 return redirect(Backend::url('backend'));
             }
